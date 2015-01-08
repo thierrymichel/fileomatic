655f78f1cb33a0bd420c87cfc11aaa48a6d1d8ad
Thierry Michel <thmichel@gmail.com>
2015-01-08 09:13:02 +0100

    change port number for Gandi (8080)

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
-/*jslint browser: true, devel: true, node: true, indent: 2 */
+/*jslint browser: true, devel: true, node: true, nomen: true */
 'use strict';
 
-require('colors');
+/*
+ * chargement modules
+ */
+require('colors'); // les logs en couleurs !
 
-var express = require('express');
-var http    = require('http');
+var express = require('express'),
+// http = require('http'),
+    morgan = require('morgan'), // http request logger (logs auto des requêtes)
+    app = express();
+// var server = http.createServer(app);
 
-var app = express();
-var server = http.createServer(app);
+app.set('port', process.env.PORT || 8080);
+app.set('view engine', 'jade');
 
-app.set('port', process.env.PORT || 3000);
+var devMode = 'development' === app.get('env');
+
+app.use(morgan(devMode ? 'dev' : 'common'));
+app.use(express.static(__dirname + '/public'));
+
+app.locals.title = 'MisterQ';
+app.locals.pretty = devMode;
+
 
 require('./controllers/home')(app);
+require('./controllers/queue')(app);
 
-server.listen(app.get('port'), function () {
-  console.log('✔︎︎ Express server listening on http://localhost:%d/'.green, app.get('port'));
+// server.listen(app.get('port'), function () {
+app.listen(app.get('port'), function () {
+    console.log('✔︎︎ Express server listening on http://localhost:%d/'.green, app.get('port'), process.versions);
 });
